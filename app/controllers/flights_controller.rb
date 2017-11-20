60a77e074c41157a14a216f0218ca3a6d7f85432
Arithmetics <brock.m.tillotson@gmail.com>
2017-11-20 10:22:16 -0800

    flight searcher working well

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -7,16 +7,39 @@ class FlightsController < ApplicationController
     @airport_options.unshift(["",""])
     if params[:flight]
       if params[:flight][:from_airport] != ""
+        #filter results
         @from_code = params[:flight][:from_airport]
         @flights = @flights.where(from_airport: Airport.find_by_code(@from_code) )
       end
 
       if params[:flight][:to_airport] != ""
+        #filter results
         @to_code = params[:flight][:to_airport]
         @flights = @flights.where(to_airport: Airport.find_by_code(@to_code) )
-        @regdate =  Date.new(params["date(1i)"].to_i,
-                    params["date(2i)"].to_i,
-                    params["date(3i)"].to_i)
+      end
+
+      if params[:flight]["start_date(1i)"] != ""
+        #default select values
+        @one = params[:flight]["start_date(1i)"].to_i
+        @two = params[:flight]["start_date(2i)"].to_i
+        @three = params[:flight]["start_date(3i)"].to_i
+        @submitted_start_date = Date.new(@one, @two, @three)
+        @flights = @flights.where("departure > ?", @submitted_start_date)
+      end
+
+      if params[:flight]["end_date(1i)"] != ""
+        #default select values
+        @one = params[:flight]["end_date(1i)"].to_i
+        @two = params[:flight]["end_date(2i)"].to_i
+        @three = params[:flight]["end_date(3i)"].to_i
+        @submitted_end_date = Date.new(@one, @two, @three)
+        #filter results
+        @flights = @flights.where("departure < ?", @submitted_end_date)
+      end
+
+      if params[:flight][:submitted_num_passengers] != ""
+        #default select values
+        @submitted_num_passengers = params[:flight][:num_passengers]
       end
 
     end
